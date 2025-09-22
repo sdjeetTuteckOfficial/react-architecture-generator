0a4deb349db27f881e2d5e27553b0a99b1a53db8
shubhajeet <shubhajeet.das@tuteck.com>
2025-09-22 16:22:23 +0530

    yoyo

--- src/components/AppFlowCanvasWrapper.jsx
+++ src/components/AppFlowCanvasWrapper.jsx
@@ -5,9 +5,8 @@ import FlowCanvas from './FlowCanvas'; // Adjust path if necessary
 // This wrapper now accepts props and passes them down
 export default function AppFlowCanvasWrapper(props) {
   return (
-    <ReactFlowProvider>
-      {/* Pass all received props to FlowCanvas */}
-      <FlowCanvas {...props} />
-    </ReactFlowProvider>
+    // <ReactFlowProvider>
+    <FlowCanvas {...props} />
+    // </ReactFlowProvider>
   );
 }
